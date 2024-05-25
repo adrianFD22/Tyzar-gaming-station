4bb241d0e31d579c36a03fdbb037da0fd774f60c
adrianFD22 <adrianfd22399@gmail.com>
2024-05-26 00:46:28 +0200

    Refactor menu.c

--- games/Menu/menu.c
+++ games/Menu/menu.c
@@ -10,6 +10,17 @@
 #define incrementar_var(x, top) (x) = (x) + 1 <= (top) ? (x) + 1 : (x)
 #define decrementar_var(x, bottom) (x) = (x) - 1 >= (bottom) ? (x) - 1 : (x)
 
+enum game {
+    N4S,
+    AHORCADO,
+    NIM,
+    PERMUTER,
+    CCP,
+    MAZE,
+    FLIPIT,
+    SALIR
+};
+
 void inicioLetras() {
     int term_size_x = getmaxx(stdscr);   // para centrar
     int term_size_y = getmaxy(stdscr);   // la pantalla
@@ -42,13 +53,14 @@ void inicioLetras() {
 }
 
 int main() {
-    setlocale(LC_ALL, ""); // caracteres especiales
+    setlocale(LC_ALL, "");  // caracteres especiales
 
     // Inicializar
-    initscr();  // Inicializa ncurses
-    cbreak();   // Leer caracteres sin esperar \n
-    noecho();   // Los caracteres introducidos por el usuario no se muestran
-    keypad(stdscr, TRUE);  // Para usar las teclas de flecha del teclado
+    initscr();              // Inicializa ncurses
+    cbreak();               // Leer caracteres sin esperar \n
+    noecho();               // Los caracteres introducidos por el usuario no se muestran
+    keypad(stdscr, TRUE);   // Para usar las teclas de flecha del teclado
+    curs_set(0);            // Oculta el cursor
 
     // Definir colores
     start_color();
@@ -58,9 +70,9 @@ int main() {
     init_pair(3, COLOR_GREEN, -1); // Para el color verde
 
     // Opciones del menú
-    char *opciones[] = { 
+    char *opciones[] = {
         "Need 4 Speed",
-        "Ahorcado", 
+        "Ahorcado",
         "NIM",
         "Xx_Permuter_xX",
         "Colorea Como Puedas: Digital Edition 2024",
@@ -92,110 +104,103 @@ int main() {
     int term_size_y = getmaxy(stdscr);
     int to_down2 = (term_size_y - 1)/2;
 
-    const char *variantes[] = { // para funcionalidad permuter
+    char *variantes_permuter[] = { // para funcionalidad permuter
         "Xx_Permuter_xX", "Xx_ePrmuter_xX", "Xx_erPmuter_xX", "Xx_ermPuter_xX",
         "Xx_ermuPter_xX", "Xx_ermutPer_xX", "Xx_ermutePr_xX", "Xx_ermuterP_xX"
     };
 
-    const char *text = "Need 4 Speed"; // para funcoinalidad N4S
-    int text_len = strlen(text);
+    const char *n4s_text = "Need 4 Speed"; // para funcoinalidad N4S
+    int text_len = strlen(n4s_text);
 
     // Ciclo del menú
     while (1) {
         // Limpiar pantalla y mostrar opciones
         clear();
         for (int i = 0; i < 8; i++) {
-            if (seleccion == i) {
-                attron(A_REVERSE);  // Inversión de color al pasar por una opción
+            if (seleccion != i) {
+                mvprintw(i + 1, 1, "%s", opciones[i]);
             }
-            mvprintw(i + 1, 1, "%s", opciones[i]);
+        }
+
+        attron(A_REVERSE);
+        switch (seleccion) {
 
             // velocidad en need 4 speed
-            if (seleccion == 0) {
-                    for (int i = 0; i < COLS - text_len; i = i + 10) {
-                        mvprintw(1, i, text);
-                        refresh();
-                        usleep(5000);
-                        mvprintw(1, i, "             ");
-                    }
-                    attron(A_REVERSE);
-                    mvprintw(1, 1, text); // Posición final
-                    attroff(A_REVERSE);
+            case N4S:
+                attroff(A_REVERSE);
+                for (int i = 0; i < COLS - text_len; i = i + 10) {
+                    mvprintw(N4S+1, i, n4s_text);
                     refresh();
-            }
-            // volver loca la opción crazy flip it
-            if (seleccion == 6) {
+                    usleep(5000);
+                    mvprintw(1, i, "             ");
+                }
                 attron(A_REVERSE);
-                mvprintw(7, 0, " ʇı dılɟ ʎzɐɹɔ");
-            }
+                mvprintw(N4S+1, 1, n4s_text); // Posición final
+                break;
+
             // ahorcado tipografía del infierno
-            if (seleccion == 1) {
-                attron(A_REVERSE);
+            case AHORCADO:
                 attron(COLOR_PAIR(1));
-                mvprintw(2, 1, "คђ๏гςค๔๏");
+                mvprintw(AHORCADO+1, 1, "คђ๏гςค๔๏");
                 //mvprintw(2, 1, "ą̴̰̤̹͚̗̑͊̆̊h̵̠̟̤̱̳͙̬͇͋͗̓̚o̸͈̣̲͗̂͋̔̓̿̔́̇͘̚͝ŗ̵̛̛̠͕̩̜̦͔̦̩̻͚͒̽͋̀̾͆̊̚͘c̸̢̫̠͎̬̖͎̼̩͑̽̊̑̌͆́̋̊̎̕̕͝͠ą̸͈̪̫̰͇̻̺̈́̒̓͒́͛̎͒͛̆͜͠d̷̟̗̙̰̲̅͐̈́̓̚̚͝o̵̡̡̳̜̟̻͙͎͋͂̄̂̀̊̇̍̿͆̂̌̾̄");
                 attroff(COLOR_PAIR(1));
-            }
+                break;
+
             // NIM parpadeante
-            if (seleccion == 2) {
-                attron(A_REVERSE);
+            case NIM:
                 attron(A_BLINK);
-                mvprintw(3, 1, "NIM");
+                mvprintw(NIM+1, 1, "NIM");
                 attroff(A_BLINK);
-            }
+                break;
+
             // Permutter permutado, secuencia de permutaciones
-            if (seleccion == 3) {
-                attron(A_REVERSE);
-                const char *texto = variantes[paridad % 8];
-                mvprintw(4, 1, texto);
-                opciones[3] = texto;
-            }
+            case PERMUTER:
+                mvprintw(PERMUTER+1, 1, variantes_permuter[paridad]);
+                opciones[3] = variantes_permuter[paridad];
+                break;
 
             // CCP con colorinchis
-            if (seleccion == 4) {
-                //attron(A_REVERSE);
+            case CCP:
+                attroff(A_REVERSE);
+                attron(COLOR_PAIR(1));      mvprintw(5, 1, "Colorea");
+                attroff(COLOR_PAIR(1));     mvprintw(5, 8, "--");
+                attron(COLOR_PAIR(2));      mvprintw(5, 10, "Como");
+                attroff(COLOR_PAIR(2));     mvprintw(5, 14, "--");
+                attron(COLOR_PAIR(3));      mvprintw(5, 16, "Puedas:");
+                attroff(COLOR_PAIR(3));     mvprintw(5, 23, "--");
+                attron(COLOR_PAIR(1));      mvprintw(5, 25, "Digital");
+                attroff(COLOR_PAIR(1));     mvprintw(5, 32, "--");
+                attron(COLOR_PAIR(2));      mvprintw(5, 34, "Edition");
+                attroff(COLOR_PAIR(2));     mvprintw(5, 41, "--");
+                attron(COLOR_PAIR(1));      mvprintw(5, 43, "2024");
+                attroff(COLOR_PAIR(1));
+                break;
 
-                attron(COLOR_PAIR(1));      // se viene troncho infumable
-                mvprintw(5, 1, "Colorea");
-                attroff(COLOR_PAIR(1));
-                mvprintw(5, 8, "--");
-                attron(COLOR_PAIR(2));
-                mvprintw(5, 10, "Como");
-                attroff(COLOR_PAIR(2));
-                mvprintw(5, 14, "--");
-                attron(COLOR_PAIR(3));
-                mvprintw(5, 16, "Puedas:");
-                attroff(COLOR_PAIR(3));
-                mvprintw(5, 23, "--");
-                attron(COLOR_PAIR(1));
-                mvprintw(5, 25, "Digital");
-                attroff(COLOR_PAIR(1));
-                mvprintw(5, 32, "--");
-                attron(COLOR_PAIR(2));
-                mvprintw(5, 34, "Edition");
-                attroff(COLOR_PAIR(2));
-                mvprintw(5, 41, "--");
-                attron(COLOR_PAIR(1));
-                mvprintw(5, 43, "2024");
-                attroff(COLOR_PAIR(1));
-            }
-            /*
             // MAZE mazed
-            if (seleccion == 5) {
+            case MAZE:
+                mvprintw(MAZE+1, 1, "MAZE");
+                break;
                 //attron(A_REVERSE);
-                mvprintw(3, 1, "NIM                _______________");
+                /* mvprintw(3, 1, "NIM                _______________");
                 mvprintw(4, 1, "Xx_Permuter_xX     | ↓ ← ← ← ← ← |");
                 mvprintw(5, 1, "Colorea Como Puedas: ↓ Digital ↑ Edition 2024");
                 mvprintw(6, 1, "MAZE     ← ← ← ← ← | ↓ |     | ↑ ←  #");
                 mvprintw(7, 1, "crazy flip it ˉ| ↑ ← ← |     ˉˉˉˉˉˉˉˉˉ");
                 mvprintw(8, 1, "Salir          ˉˉˉˉˉˉˉˉˉ");
                 attron(A_REVERSE);
-                mvprintw(6, 1, "MAZE");
-            }
-            */
+                mvprintw(6, 1, "MAZE"); */
 
-            attroff(A_REVERSE);  // Quita la inversión de color
+            // volver loca la opción crazy flip it
+            case FLIPIT:
+                mvprintw(FLIPIT+1, 0, " ʇı dılɟ ʎzɐɹɔ");
+                break;
+
+            case SALIR:
+                mvprintw(SALIR+1, 1, "Salir");
+                break;
         }
+
+        attroff(A_REVERSE);
         refresh(); // Actualizar la pantalla
 
         opcion = getch(); // Capturar entrada del usuario
@@ -203,13 +208,13 @@ int main() {
             case 'k':
             case KEY_UP:
                 decrementar_var(seleccion, 0);
-                if (seleccion == 3) paridad++; // para permuter
+                if (seleccion == PERMUTER) paridad = (paridad+1) % 8; // para permuter
                 break;
 
             case 'j':
             case KEY_DOWN:
                 incrementar_var(seleccion, opciones_len-1);
-                if (seleccion == 3) paridad++; // para permuter
+                if (seleccion == PERMUTER) paridad = (paridad+1) % 8; // para permuter
                 break;
 
             case 'q':
